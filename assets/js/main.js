db649260d27fa04cf098eb7d8a289040049cc44f
Moley456 <kingstarwei10@gmail.com>
2022-02-25 18:27:55 +0800

    complete About, Skills, Qualification, Services, Portfolio sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,93 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close');
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
-  });
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  });
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav_link');
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu');
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu');
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    if (itemClass === 'skills_content skills_close') {
+        this.parentNode.className = 'skills_content skills_open';
+    } else if (itemClass === 'skills_content skills_open') {
+        this.parentNode.className = 'skills_content skills_close';
+    }
+}
+
+skillsHeader.forEach((element) => {
+    element.addEventListener('click', toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services_modal'),
+    modalBtns = document.querySelectorAll('.services_button'),
+    modalCloses = document.querySelectorAll('.services_modal-close');
 
+let openModal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+};
+
+let closeModal = function (modalClick) {
+    modalViews[modalClick].classList.remove('active-modal');
+};
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        openModal(i);
+    });
+});
+
+modalCloses.forEach((modalClose, i) => {
+    modalClose.addEventListener('click', () => {
+        closeModal(i);
+    });
+});
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
